f5a92651f2512be850112c16548a9c6aa7ed7f09
Milena <milenavasconcelos.info@gmail.com>
2025-06-09 12:25:00 -0400

    feat:  basename added in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-   <BrowserRouter>
+   <BrowserRouter basename="/Personagens-Rick-And-Morty">
     <App />
   </BrowserRouter>
 )
